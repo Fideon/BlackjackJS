d6858aea7f396a145dea22520fa305acc1a407e7
Fidel Salazar <fsf91@hotmail.com>
2018-12-24 18:36:14 -0600

    Added finishing functions and touches

--- script.js
+++ script.js
@@ -1,25 +1,62 @@
 /// BlackJack
 
+//Cards
 let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
 let values = ['Ace', 'King', 'Queen', 'Jack',
               'Ten', 'Nine', 'Eight', 'Seven',
               'Six', 'Five', 'Four', 'Three', 'Two',];
 
-let textArea = document.getElementById('txtArea');
-let btnNewGame = document.getElementById('btnNewGame');
-let btnHit = document.getElementById('btnHit');
-let btnStay = document.getElementById('btnStay');
+//DOM Variables
+let textArea = document.getElementById('txtArea'),
+    btnNewGame = document.getElementById('btnNewGame'),
+    btnHit = document.getElementById('btnHit'),
+    btnStay = document.getElementById('btnStay');
+    
+//Game Variables
+let gameStarted = false,
+    gameOver = false,
+    playerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore = 0,
+    deck = [];
 
 btnHit.style.display = 'none';
 btnStay.style.display = 'none';
+showStatus();
 
 btnNewGame.addEventListener('click', function() 
 {
-  textArea.innerText = 'Started...';
+  gameStarted = true;
+  gameOver = false;
+  playerWon = false;
+  
+  deck = createDeck();
+  shuffleDeck(deck);
+  dealerCards = [getNextCard(), getNextCard()];
+  playerCards = [getNextCard(), getNextCard()];
+  
+  
   btnNewGame.style.display = 'none';
   btnHit.style.display = 'inline';
   btnStay.style.display = 'inline';
-})
+  showStatus();
+});
+
+btnHit.addEventListener('click', function() 
+{
+  playerCards.push(getNextCard());
+  checkForEndOfGame();
+  showStatus();
+});
+
+btnStay.addEventListener('click', function() 
+{
+  gameOver = true;
+  checkForEndOfGame();
+  showStatus();
+});
 
 function createDeck()
 {
@@ -40,24 +77,150 @@ function createDeck()
   return deck;
 }
 
+function getCardNumericValue(card)
+{
+  switch(card.value)
+  {
+    case 'Ace':
+      return 1;
+    case 'Two':
+      return 2;
+    case 'Three':
+      return 3;
+    case 'Four':
+      return 4;
+    case 'Five':
+      return 5;
+    case 'Six':
+      return 6;
+    case 'Seven':
+      return 7;
+    case 'Eight':
+      return 8;
+    case 'Nine':
+      return 9;
+    default:
+      return 10;
+  }
+}
+
+function getScore(cardArray)
+{
+  let score = 0,
+      hasAce = false;
+      for (let i = 0; i < cardArray.length; i++)
+      {
+        let card = cardArray[i];
+        score += getCardNumericValue(card);
+        if(card.value === "Ace")
+          hasAce = true;
+      }
+      if(hasAce && score + 10 <= 21)
+        return score + 10;
+      
+      return score;
+}
+
+function shuffleDeck(deck)
+{
+  for(let i = 0; i < deck.length; i++)
+  {
+    let swap = Math.trunc(Math.random() * deck.length);
+    let tmp = deck[swap];
+    deck[swap] = deck[i];
+    deck[i] = tmp;
+  }
+}
+
 function getNextCard()
 {
   return deck.shift();
 }
-
-let deck = createDeck();
 
 function getCardString(card)
 {
   return card.value + " of " + card.suit;
 }
 
-//let card1 = "Ace of Spades",
-//    card2 = "Ten of Hearts";
+function updateScores()
+{
+  dealerScore = getScore(dealerCards);
+  playerScore = getScore(playerCards);
+}
 
-let playerCards = [getNextCard(), getNextCard()];
-    
-console.log("Welcome to Blackjack!");
-console.log("You are dealt: ");
-console.log(" " + getCardString(playerCards[0]));
-console.log(" " + getCardString(playerCards[1]));
+function checkForEndOfGame()
+{
+  updateScores();
+  
+  if(gameOver)
+  {
+    while(dealerScore < playerScore && playerScore <= 21 
+          && dealerScore <= 21) 
+    {
+      dealerCards.push(getNextCard());
+      updateScores();
+    }
+  }
+  
+  if (playerScore > 21)
+  {
+    playerWon = false;
+    gameOver = true;
+  }
+  else if (dealerScore > 21)
+  {
+    playerWon = true;
+    gameOver = true;
+  }
+  else if (gameOver)
+  {
+    if(playerScore > dealerScore)
+      playerWon = true;
+    else
+      playerWon = false;
+  }
+}
+
+function showStatus()
+{
+  if (!gameStarted)
+  {
+    textArea.innerText = "Welcome to Blackjack!";
+    return;
+  }
+  
+  let dealerCardString = "";
+  for (let i = 0; i < dealerCards.length; i++)
+  {
+    dealerCardString += getCardString(dealerCards[i]) + "\n";
+  }
+  
+  let playerCardString = "";
+  for (let i = 0; i < playerCards.length; i++)
+  {
+    playerCardString += getCardString(playerCards[i]) + "\n";
+  }
+  
+  updateScores();
+  
+  textArea.innerText =
+  "Dealer has:\n" +
+  dealerCardString +
+  "(score: " + dealerScore + ")\n\n" +
+  
+  "Player has:\n" +
+  playerCardString +
+  "(score: " + playerScore + ")\n\n";
+  
+  if(gameOver)
+  {
+    if(playerWon)
+      textArea.innerText += "YOU WON!";
+    else
+      textArea.innerText += "Dealer wins!";
+      
+    btnNewGame.style.display = 'inline';
+    btnHit.style.display = 'none';
+    btnStay.style.display = 'none';
+  }
+}
